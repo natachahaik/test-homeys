40c1d9843e19891292ee1ba4a93093271f3c8e93
natachahaik <103328538+natachahaik@users.noreply.github.com>
2022-12-15 11:26:48 +0100

    step 1: integration BaseNotification

---
+++ components/BaseNotification.vue
@@ -0,0 +1,73 @@
+<template>
+
+<section class="container">
+<div class="container-header">
+ <span class="success">
+    <img :src="success" alt="icône success" class="success-icone"/>
+    <h1>Modal Window</h1>
+</span>
+
+<div class="close">
+    <img :src="close" alt="icône close" class="close-icone"/>
+</div>
+</div>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugiat nostrum ab aperiam molestiae maxime sit cupiditate, dolorem pariatur dolore maiores nobis. Unde reprehenderit placeat officiis aliquid, dicta beatae illum.</p>
+</section>
+
+</template>
+
+
+<script lang="ts" setup>
+import success from "@/assets/success.svg"
+import close from "@/assets/close.svg"
+
+</script>
+
+
+
+<style lang="scss" scoped>
+
+.container{
+    background-color: #e4f7e8 ;
+    width: 700px;
+    height: 215px;
+    margin: 40px;
+    border:2px solid #3b793e ;
+    border-radius: 16px;
+    padding: 25px;
+}
+
+.container-header{
+display: flex;
+justify-content: space-between;
+margin-bottom: 22px;
+margin-left: 5px;
+}
+
+.success{
+    display: flex;
+    justify-content: flex-start;
+    gap: 28px;
+}
+
+.success-icone {
+    width: 40px;
+}
+.close{
+    display: flex;
+    justify-content: end;
+}
+
+.close-icone {
+    width: 30px; 
+}
+
+p {
+    font-size: 20px;
+    margin-left: 65px;
+
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="page">
+    <base-notification />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      /**
+       * Liste des notifications à afficher pour l'étape 1
+       * @example [{
+       *   title // titre de la notification
+       *   content // contenu de la notification
+       *   variant // type de notification (sert pour déterminer quelle couleur utiliser)
+       * }]
+       */
+      notifications: [
+        {
+          title: 'Modal Window',
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor",
+          variant: 'danger',
+        },
+        {
+          title: 'Modal Window',
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor",
+          variant: 'success',
+        },
+        {
+          title: 'Modal Window',
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor",
+          variant: 'warning',
+        },
+        {
+          title: 'Modal Window',
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor",
+          variant: 'info',
+        }
+      ],
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.page {
+  position: relative;
+  min-height: 100vh;
+}
+</style>
